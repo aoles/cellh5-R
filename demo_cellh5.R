258c9c8fee9f38db871b262c62301bf3046759ec
Rudolf Hoefler <rudolf.hoefler@gmail.com>
2014-01-02 14:37:52 +0100

    VC

--- demo_cellh5.R
+++ demo_cellh5.R
@@ -21,15 +21,14 @@ frames_ <-  c(0, 1)#,1,2,3,4,5,6,7,8)
 # read out data
 object_counts <- C5ObjectCounts(c5f, positions[[1]], primary)
 features <- C5FeaturesByName(c5f, positions[[1]], primary, main_features, frames=frames_)
-object_ids <- C5ObjectLabels(positions[[1]], primary, frames=frames_)
+object_ids <- C5ObjectLabels(positions[[1]], primary)
 center <- C5Center(positions$W0_P0013, primary)
 bbox <- C5BoundingBoxes(positions$W0_P0013, primary)
 orientation <- C5Orientation(positions$W0_P0013, primary)
 predictions <- C5Predictions(c5f, positions$W0_P0013, primary)
 probs <- C5PredictionProbabilities(c5f, positions$W0_P0013, primary)
 image_ <- C5ReadImage(c5f, positions$W0_P0013, primary, frame_index=1, zstack=1)
-
 events <- C5Events(c5f, positions[[1]], primary, include_branches=TRUE, return_indices=TRUE)
 event_features <- C5EventFeatures(c5f, positions[[1]], primary, TRUE, main_features)
 # don't forget to release the resources
-#C5Close(c5f)
+# C5Close(c5f)
