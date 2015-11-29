aea3ee2195958d77ed16198efa8dbe6f4d370ea1
rhoef <rudolf.hoefler@gmail.com>
2015-11-29 14:53:57 +0100

    VC

--- demo_cellh5.R
+++ demo_cellh5.R
@@ -4,7 +4,7 @@
 library(cellh5)
 
 
-c5f <- CellH5("data/_all_positions.ch5")
+c5f <- CellH5("/home/public/imba/analysis/hdf5/_all_positions.ch5")
 chreg <- C5ChannelRegions(c5f)
 primary <- chreg[[1]] # primary__primary
 plates <- C5Plates(c5f)
@@ -17,29 +17,31 @@ classdef <- C5ClassifierDefinition(c5f, primary)
 main_features = c("n2_avg", "n2_stddev", "roisize")
 main_features <- clfeatures[c(1,2,3)]
 
+pos <- positions$W0_P0037
+
 # read out data, we need to check if there objects/cells found. The same applies
 # to the methods that read the event data. The checks are not mandatory since
 # the function return NULL if the file contains no data. For sake of readability
 # is recommended to use the functions C5HasObjecs/C5HasEvents
 
-if (C5HasObjects(positions[[1]], primary)) {
-  object_counts <- C5ObjectCounts(c5f, positions[[1]], primary)
-  features <- C5FeaturesByName(c5f, positions[[1]], primary, main_features)
-  object_ids <- C5ObjectLabels(positions[[1]], primary)
-  center <- C5Center(positions$W0_P0013, primary)
-  bbox <- C5BoundingBoxes(positions$W0_P0013, primary)
-  orientation <- C5Orientation(positions$W0_P0013, primary)
-  predictions <- C5Predictions(c5f, positions$W0_P0013, primary, as="label")
-  probs <- C5PredictionProbabilities(c5f, positions$W0_P0013, primary)
-  details <- C5ObjectDetails(c5f, positions$W0_P0013, primary)
-  contours <- C5Contours(c5f, positions$W0_P0013, primary, frame=1)
+if (C5HasObjects(pos, primary)) {
+  object_counts <- C5ObjectCounts(c5f, pos, primary)
+  features <- C5FeaturesByName(c5f, pos, primary, main_features)
+  object_ids <- C5ObjectLabels(pos, primary)
+  center <- C5Center(pos, primary)
+  bbox <- C5BoundingBoxes(pos, primary)
+  orientation <- C5Orientation(pos, primary)
+  predictions <- C5Predictions(c5f, pos, primary, as="label")
+  probs <- C5PredictionProbabilities(c5f, pos, primary)
+  details <- C5ObjectDetails(c5f, pos, primary)
+  contours <- C5Contours(c5f, pos, primary, frame=1)
 } else {
   warning("no objects found!")
 }
 
-if (C5HasEvents(positions[[1]])) {
-  events <- C5Events(c5f, positions[[1]], primary, include_branches=TRUE, return_indices=F)
-  event_features <- C5EventFeatures(c5f, positions[[1]], primary, TRUE, main_features)
+if (C5HasEvents(pos)) {
+  events <- C5Events(c5f, pos, primary, include_branches=TRUE, return_indices=F)
+  event_features <- C5EventFeatures(c5f, pos, primary, TRUE, main_features)
 } else {
   warning('no events found!')
 }
